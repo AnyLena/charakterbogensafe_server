3cd00572bb59cd64d06f6c7e324781850f3fda2d
LenaBultmann <lena.bultmann@gmx.de>
2024-06-22 23:32:23 +0200

    adding more route protection

--- routes/splittermond.js
+++ routes/splittermond.js
@@ -1,18 +1,29 @@
-import express from "express"
-import { postSplittermond, getSplittermondAll, getSplittermond, updateSplittermond, deleteSplittermond, addItem, deleteItem, postMondzeichen, getMondzeichen } from "../controller/splittermond.js"
+import express from "express";
+import {
+  postSplittermond,
+  getSplittermondAll,
+  getSplittermond,
+  updateSplittermond,
+  deleteSplittermond,
+  addItem,
+  deleteItem,
+  postMondzeichen,
+  getMondzeichen,
+} from "../controller/splittermond.js";
+import { verifyToken } from "../middlewares/verifyToken.js";
 
-const splittermondRouter = express.Router()
+const splittermondRouter = express.Router();
 
-splittermondRouter.get("/all/:id", getSplittermondAll)
-splittermondRouter.get("/:id", getSplittermond)
-splittermondRouter.post("/", postSplittermond)
-splittermondRouter.delete("/", deleteSplittermond)
-splittermondRouter.put("/", updateSplittermond)
+splittermondRouter.get("/all/:id", verifyToken, getSplittermondAll);
+splittermondRouter.get("/:id", verifyToken, getSplittermond);
+splittermondRouter.post("/", postSplittermond);
+splittermondRouter.delete("/", deleteSplittermond);
+splittermondRouter.put("/", updateSplittermond);
 
-splittermondRouter.put("/addItem", addItem)
-splittermondRouter.put("/deleteItem", deleteItem)
+splittermondRouter.put("/addItem", addItem);
+splittermondRouter.put("/deleteItem", deleteItem);
 
-splittermondRouter.post("/mondzeichen", postMondzeichen)
-splittermondRouter.get("/mondzeichen/:id", getMondzeichen)
+splittermondRouter.post("/mondzeichen", postMondzeichen);
+splittermondRouter.get("/mondzeichen/:id", getMondzeichen);
 
 export default splittermondRouter;
